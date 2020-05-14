e8bbadc62d80583bf0c3d95ff459551f523deeaf
Artem Sagdiev <sagdiev@gmail.com>
2020-05-14 20:22:43 +0300

    add pipelines

---
+++ main_pipeline_stochastic_ticker.py
@@ -0,0 +1,6 @@
+
+# основная последовательность выполнения файлов по генерированию стахастических кривых и прохождениюих ботами
+
+exec(open('stochactic_process_generator_ticker.py').read())
+exec(open('bot_prediction.py').read())
+exec(open('bot_bi_preparation.py').read())
